0950afbab1f447c86aa623ce0f41310b3ebe86c8
Mahdi <mah454@mail.moke.ir>
2025-10-11 20:14:36 +0330

    update script

--- run_openresty.sh
+++ run_openresty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-podman run -it --name lua --rm \
+podman run -it --name openresty --rm \
   --volume $PWD/nginx:/usr/local/openresty/nginx/conf/ \
   -v $PWD/lua:/opt/lua \
   -p 8080:80 \
-  registry.docker.ir/openresty/openresty:latest
+  registry.docker.ir/openresty/openresty:alpine-fat
