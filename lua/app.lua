75de95af4f44aa82f4ff14ecc9be14dec409a376
Mahdi <mah454@mail.moke.ir>
2025-10-16 18:25:47 +0330

    improvement

--- lua/app.lua
+++ lua/app.lua
@@ -1,4 +1,8 @@
+local ngx = require("ngx")
+local cjson = require("cjson")
+
 ngx.say("Hello")
+
 local headers = ngx.req.get_headers()
 
 for k, v in pairs(headers) do
--- run_openresty.sh
+++ run_openresty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 podman run -it --name openresty --rm \
-  --volume $PWD/nginx:/usr/local/openresty/nginx/conf/ \
+  -v $PWD/nginx:/usr/local/openresty/nginx/conf/ \
   -v $PWD/lua:/opt/lua \
   -p 8080:80 \
   registry.docker.ir/openresty/openresty:alpine-fat
