c85b6835a793b9ba035fceb3a2733c719a320a85
Mahdi <mah454@mail.moke.ir>
2025-10-23 14:43:12 +0330

    Improvement

--- example/app.lua
+++ example/app.lua
@@ -1,11 +1,12 @@
 package.path = "/opt/lua/?.lua;" .. package.path
 local finch = require("finch")
+local logger = require("finch.logger")
 
-local function handleRootReq(_, resp)
+local function handle(_, resp)
 	resp.header("name", "ali")
 	resp.body("Hello dear")
 end
 
-finch.route("/", "GET", handleRootReq)
+finch.route("/", "GET", handle)
 
 finch.start()
--- run_openresty.sh
+++ run_openresty.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-podman run -it --name openresty --rm \
+CMD=$(which podman)
+if [ -z $CMD ]; then
+  CMD=$(which docker)
+fi
+
+echo "[*] Use [$CMD] as container engine"
+echo "[*] Start finch container"
+$CMD run -it --rm \
+  --name finch \
   -v $PWD/nginx:/usr/local/openresty/nginx/conf/ \
   -v $PWD/example:/opt/lua \
   -p 8080:80 \
